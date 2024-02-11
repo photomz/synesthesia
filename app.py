06ade48206deb293ac69cabde1198c94a5993ca3
Markus Zhang <markuszhang8@gmail.com>
2024-02-10 16:17:15 -0800

    add vercel

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from starlette.responses import JSONResponse
 from spotify import *
 
@@ -9,6 +10,30 @@ async def analyze(track_id: str):
     track_info = get_analysis(track_id)
     data = analyze_track(track_info)
     return JSONResponse(content=data)
+
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI on Vercel</title>
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            <h1>Hello from FastAPI</h1>
+            <ul>
+                <li><a href="/docs">/docs</a></li>
+                <li><a href="/redoc">/redoc</a></li>
+            </ul>
+            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
+        </div>
+    </body>
+</html>
+"""
+
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
 
 if __name__ == "__main__":
     import uvicorn
